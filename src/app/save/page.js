2987bd9c09a4787e32129ddc1a1851dba28fe822
sonniez matteo <sonniezmatteo@MacBook-Pro-de-sonniez-2.local>
2025-06-05 00:00:31 +0200

    test version final 2

--- src/app/save/page.js
+++ src/app/save/page.js
@@ -13,12 +13,11 @@ function Page() {
   const [isReveal, setIsReveal] = useState(false);
 
   // Refs pour gérer la progression
-  const progressRef = useRef(0);       // Avancement de l'auto-scroll [0..1]
-  const manualOffsetRef = useRef(0);   // Avancement manuel via molette
-  const totalProgressRef = useRef(0);  // somme (auto-scroll + manuel)
+  const progressRef = useRef(0); // Avancement de l'auto-scroll [0..1]
+  const manualOffsetRef = useRef(0); // Avancement manuel via molette
+  const totalProgressRef = useRef(0); // somme (auto-scroll + manuel)
   const testRef = useRef(null);
-  const testRef2 = useRef(null);      // Elément qui se déplace sur la barre noire
-
+  const testRef2 = useRef(null); // Elément qui se déplace sur la barre noire
 
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
@@ -154,15 +153,15 @@ function Page() {
           const x = -scrollWidth * wrappedProgress;
           const offsetPx = window.innerWidth * 0.2;
 
-    // défilement horizontal infini du container,
-    // en partant de offsetPx (20vw) puis en décroissant
+          // défilement horizontal infini du container,
+          // en partant de offsetPx (20vw) puis en décroissant
           //const x = offsetPx - scrollWidth * wrappedProgress;
           gsap.set(containerRef.current, { x });
 
           // Déplacement de testRef sur la barre noire
           if (testRef.current) {
             const parentWidth = window.innerWidth; // largeur de la fenêtre
-            const testWidth = parentWidth * 0.1;   // car w-[10vw]
+            const testWidth = parentWidth * 0.1; // car w-[10vw]
 
             // On part de x = -testWidth (bord gauche, hors écran)
             // à x = parentWidth (bord droit, hors écran)
@@ -175,8 +174,6 @@ function Page() {
         }
       },
     });
-
-    
 
     const handleWheel = (e) => {
       e.preventDefault();
@@ -196,58 +193,54 @@ function Page() {
 
   return (
     <div>
-
-      {/* MOBILE VERSION */}
-      <div className="block sm:hidden testup">
       <div className="relative w-[28vw] h-[68vh] flex-shrink-0 ml-[5vw] mt-[9vh]">
-            <Image src="/img26.webp" alt="" fill className="object-cover" />
-          </div>
+        <Image src="/img26.webp" alt="" fill className="object-cover" />
+      </div>
 
-          <div className="relative w-[20vw] h-[50vh] flex-shrink-0  ml-[3vw]">
-            {/* border-[2px] border-slate-600 */}
-            <Image src="/img8.webp" alt="" fill className="object-cover" />
+      <div className="relative w-[20vw] h-[50vh] flex-shrink-0  ml-[3vw]">
+        {/* border-[2px] border-slate-600 */}
+        <Image src="/img8.webp" alt="" fill className="object-cover" />
+      </div>
+      <div className="flex flex-col ml-[5vw]">
+        <div className="flex ml-[6vw] space-x-[1.5vw]">
+          <div className="relative w-[15vw] h-[35vh] flex-shrink-0">
+            <Image src="/img23.webp" alt="" fill className="object-cover" />
           </div>
-          <div className="flex flex-col ml-[5vw]">
-            <div className="flex ml-[6vw] space-x-[1.5vw]">
-              <div className="relative w-[15vw] h-[35vh] flex-shrink-0">
-                <Image src="/img23.webp" alt="" fill className="object-cover" />
-              </div>
-              <div className="relative w-[15vw] h-[35vh] flex-shrink-0">
-                <Image src="/img24.webp" alt="" fill className="object-cover" />
-              </div>
-              <div className="relative w-[15vw] h-[35vh] flex-shrink-0">
-                <Image src="/img25.webp" alt="" fill className="object-cover" />
-              </div>
-            </div>
-            <div className="relative w-[35vw] h-[38vh] flex-shrink-0 mt-[5vh]">
-              <Image src="/img5.webp" alt="" fill className="object-cover" />
-            </div>
+          <div className="relative w-[15vw] h-[35vh] flex-shrink-0">
+            <Image src="/img24.webp" alt="" fill className="object-cover" />
           </div>
-          <div className="relative w-[28vw] h-[65vh] flex-shrink-0 ml-[3vw]">
-            <Image src="/img12.webp" alt="" fill className="object-cover" />
+          <div className="relative w-[15vw] h-[35vh] flex-shrink-0">
+            <Image src="/img25.webp" alt="" fill className="object-cover" />
           </div>
-          <div className="relative w-[16vw] h-[35vh] flex-shrink-0 ml-[3vw] mt-[40vh]">
-            <Image src="/img1.webp" alt="" fill className="object-cover" />
-          </div>
-          {/* border-[2px] border-slate-600 */}
-          <div className="relative w-[30vw] h-[35vh] flex-shrink-0 mb-[40vh] -ml-[10vw]">
-            <Image src="/img7.webp" alt="" fill className="object-cover" />
-          </div>
-          {/* border-[2px] border-slate-600 */}
-          <div className="relative w-[30vw] h-[35vh] flex-shrink-0 -mb-[5vh] -ml-[10vw]">
-            <Image src="/img4.webp" alt="" fill className="object-cover" />
-          </div>
-          <div className="relative w-[30vw] h-[35vh] flex-shrink-0 mt-[42vh] -ml-[10vw]">
-            <Image src="/img17.webp" alt="" fill className="object-cover" />
-          </div>
-          <div className="relative w-[16vw] h-[35vh] flex-shrink-0 mb-[36vh] -ml-[10vw]">
-            <Image src="/img19.webp" alt="" fill className="object-cover" />
-          </div>
-          <div className="relative w-[22vw] h-[50vh] flex-shrink-0 ml-[4vw]">
-            <Image src="/img21.webp" alt="" fill className="object-cover" />
-          </div>
+        </div>
+        <div className="relative w-[35vw] h-[38vh] flex-shrink-0 mt-[5vh]">
+          <Image src="/img5.webp" alt="" fill className="object-cover" />
+        </div>
+      </div>
+      <div className="relative w-[28vw] h-[65vh] flex-shrink-0 ml-[3vw]">
+        <Image src="/img12.webp" alt="" fill className="object-cover" />
+      </div>
+      <div className="relative w-[16vw] h-[35vh] flex-shrink-0 ml-[3vw] mt-[40vh]">
+        <Image src="/img1.webp" alt="" fill className="object-cover" />
+      </div>
+      {/* border-[2px] border-slate-600 */}
+      <div className="relative w-[30vw] h-[35vh] flex-shrink-0 mb-[40vh] -ml-[10vw]">
+        <Image src="/img7.webp" alt="" fill className="object-cover" />
+      </div>
+      {/* border-[2px] border-slate-600 */}
+      <div className="relative w-[30vw] h-[35vh] flex-shrink-0 -mb-[5vh] -ml-[10vw]">
+        <Image src="/img4.webp" alt="" fill className="object-cover" />
+      </div>
+      <div className="relative w-[30vw] h-[35vh] flex-shrink-0 mt-[42vh] -ml-[10vw]">
+        <Image src="/img17.webp" alt="" fill className="object-cover" />
+      </div>
+      <div className="relative w-[16vw] h-[35vh] flex-shrink-0 mb-[36vh] -ml-[10vw]">
+        <Image src="/img19.webp" alt="" fill className="object-cover" />
+      </div>
+      <div className="relative w-[22vw] h-[50vh] flex-shrink-0 ml-[4vw]">
+        <Image src="/img21.webp" alt="" fill className="object-cover" />
+      </div>
     </div>
-
   );
 }
 
